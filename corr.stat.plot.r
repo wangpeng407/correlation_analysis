3a4a615195de60a03b00ca5b8e4e4749318ae67c
WP <31948527+wangpeng407@users.noreply.github.com>
2022-01-21 17:21:54 +0800

    Update corr.stat.plot.r

--- corr.stat.plot.r
+++ corr.stat.plot.r
@@ -1,6 +1,7 @@
 library(optparse)
 option_list <- list(
   make_option(c("--taxa_abund"), action="store", type="character", default=NULL, help="Input the taxa abundance table, coloums are samples, row are taxa"),
+  make_option(c("--clr"), action="store_true", default=FALSE, help="if transform data using center log ratio"),
   make_option(c("--pheno"), action="store", type="character", default=NULL, help="Input the pheno data or env data or clinical data"),
   make_option(c("--taxa_info"), action="store", type="character", default=NULL, help = "input the taxa_info, first colum is id and second column is map[Phylum]"),
   make_option(c("--na_percent"), action="store", default=0.1, type="double", help="if one pheno contains 10% NA, then remove it, default is 0.1"),
@@ -9,7 +10,8 @@ option_list <- list(
   make_option(c("--top_taxa"), action="store", default=100, type="integer", help="choose top 100 taxa for subsequent analysis"),
   make_option(c("--corr_method"), action="store", default='spearman', type="character", help="the method of correlation, default is spearman"),
   make_option(c("--prefix"), action="store", default=NULL, type="character", help="input the prefix of output files"),
-  make_option(c("--outdir"), action="store", default="./", type="character", help="The output dirctory, default is ./")
+  make_option(c("--outdir"), action="store", default="./", type="character", help="The output dirctory, default is ./"),
+  make_option(c("--distance"), action="store", default="bray", type="character", help="The output dirctory, default is bray, other choices are manhattan, euclidean, canberra, clark, bray, kulczynski, jaccard, gower, altGower, morisita, horn, mountford, raup, binomial, chao, cao or mahalanobi")
 )
 opt <- parse_args(OptionParser(usage="%prog [options] file\n", option_list=option_list))
 
@@ -47,17 +49,19 @@ for(fl in c(opt$taxa_abund, opt$pheno)){
 }
 if(is.na(file.info(opt$outdir)$isdir)) dir.create(opt$outdir, recursive = TRUE)
 
-micro <- read.table(opt$taxa_abund, sep = '\t', header = T, row.names = 1, stringsAsFactors = F)
+micro <- read.table(opt$taxa_abund, sep = '\t', header = T, row.names = 1, stringsAsFactors = F, comment.char = "", check.names = FALSE)
 
+clr.micro <- as.data.frame(apply(micro+1, 2, clr))
 #if(!is.null(opt$trans)){micro <- as.data.frame(t(micro))}
 
-pheno <- read.table(opt$pheno, sep = '\t', header = T, blank.lines.skip =  F, stringsAsFactors = F)
+pheno <- read.table(opt$pheno, sep = '\t', header = T, row.names = 1, blank.lines.skip =  F, stringsAsFactors = F)
 
 env.name <- colnames(pheno)
 #remove vars contain 10% nas
 clean_pheno <- pheno[, apply(pheno, 2, na.prop) < as.numeric(opt$na_percent)]
 #numeric transform
 clean_pheno <- as.data.frame(apply(clean_pheno, 2, as.numeric))
+rownames(clean_pheno) <- rownames(pheno)
 #fill NAs
 clean_pheno <- as.data.frame(apply(clean_pheno, 2, na.replace))
 #remove non-varied variables
@@ -94,6 +98,12 @@ if(is.integer(opt$top_taxa)){
 }else{
   top_micro <- as.data.frame(t(clean_micro))
 }
+
+if(opt$clr){
+	top_micro  <- clr.micro[rownames(clr.micro) %in% colnames(top_micro), ]
+	top_micro <- t(top_micro)
+	cat("Perform CLR transformation...\n")
+}
 if(!is.null(opt$taxa_info)) {
 	taxa_map <- read.table(opt$taxa_info, sep = '\t', header = F, stringsAsFactors = F)
 	colnames(taxa_map) <- c('taxa', 'map')
@@ -107,21 +117,28 @@ if(!is.null(opt$taxa_info)) {
 	annotation <- ann_col <- NULL	
 }
 #caculate correlation matrix
+#write.table("haha.xls",file=top_micro, sep="\t", quote=F, row.names = F)
 if(!is.null(numerical_pheno)){
 	res <- corr.test(top_micro, numerical_pheno, method = opt$corr_method, adjust = 'BH')
 	rmat <- res$r
 	pmat <- res$p
+	opt$prefix <- ifelse(is.null(opt$prefix), '', paste0(opt$prefix, '.'))
+	rout <- paste(opt$outdir, '/', opt$prefix, 'numerical.cor.r.csv', sep = '')
+	pout <- paste(opt$outdir, '/', opt$prefix, 'numerical.cor.p.csv', sep = '')
+	write.csv(rmat, rout, quote = F)
+	write.csv(pmat, pout, quote = F)
 	x.n <- ncol(rmat)
 	y.n <- nrow(rmat)
-	width1 <- 8 + 0.2 * x.n
-	height <- 10 + 0.1 * y.n
-	opt$prefix <- ifelse(is.null(opt$prefix), '', paste0(opt$prefix, '.'))
+	fold <- max(nchar(rownames(rmat))) / 5
+	width1 <- (8 + 0.2 * x.n ) + fold/1.5
+	height <- (10 + 0.2 * y.n) 
 	outfig1 <- paste(opt$outdir, '/', opt$prefix, 'numerical.cor.heat.pdf', sep = '')
-	pheatmap(rmat, color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
+	pheatmap(rmat, 
+			#color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
          	display_numbers = matrix(ifelse(pmat>0.01&pmat<0.05, '*',ifelse(pmat<=0.01, '**', ' ')),nrow = nrow(pmat)),
-	        annotation_row = annotation, border_color = NA, fontsize=12, fontsize_number=10,
-	        annotation_colors = list(Note = ann_col), number_color = 'black',
-		    filename = outfig1, width = width1, height = height)
+	        annotation_row = annotation, border_color = NA, fontsize=15, fontsize_number=20,number_color='grey20',
+	        annotation_colors = list(Note = ann_col),
+		    filename = outfig1, width = width1, height = height, limitsize = FALSE)
 }
 
 if(!is.null(binary_pheno)){
@@ -136,19 +153,21 @@ if(!is.null(binary_pheno)){
 	p.mat <- as.data.frame(t(wres.list$PMAT))
 	x.n <- ncol(wmat)
 	y.n <- nrow(wmat)
-	width <- 8 + 0.2 * x.n 
-	height <- 10 + 0.1 * y.n
+	fold <- max(nchar(rownames(rmat))) / 5
+	width <- (8 + 0.2 * x.n ) + fold/1.5
+	height <- (10 + 0.2 * y.n) 
 	outfig3 <- paste(opt$outdir, '/', opt$prefix, 'binary.diff.heat.pdf', sep = '')
-	pheatmap(wmat, scale = 'column', color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
+	pheatmap(wmat, scale = 'column', 
+		 #color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
          display_numbers = matrix(ifelse(p.mat>0.01&p.mat<0.05, '*',ifelse(p.mat<=0.01, '**', ' ')),nrow = nrow(p.mat)),
          number_color = 'black', annotation_row = annotation, border_color = 'NA',
-         annotation_colors = list(Note = ann_col), fontsize=12, fontsize_number=3,
-         filename = outfig3, width = width, height = height)
+         annotation_colors = list(Note = ann_col), fontsize=15, fontsize_number=20, number_color='grey20',
+         filename = outfig3, width = width, height = height, limitsize = FALSE)
 }
 size = 15
 if(!is.null(numerical_pheno)){
-	pca <- rda(top_micro, scale = T)
-	nmds <- metaMDS(top_micro, distance = 'bray', k=2, try = 100, trace = 0)
+	pca <- tryCatch(rda(top_micro, scale = T))
+	nmds <- tryCatch(metaMDS(top_micro, distance = opt$distance, k=2, try = 100, trace = 0))
 	for(res in c('pca', 'nmds')){
 		tmp <- get(res)
   		evnfit.res <- envfit(tmp, numerical_pheno)
